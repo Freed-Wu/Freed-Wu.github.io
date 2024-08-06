6ea12ba309a2031028ed6567b6d912b83ee8816a
Freed-Wu <Freed-Wu@users.noreply.github.com>
2024-08-06 06:32:41 +0000

    jekyll build from Action cc460e9a02af30fb8675f45a2dc3302ac0d7e4d1

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+with rubyPackages;
+mkShell {
+  packages = [
+    jekyll-archives
+    jekyll-avatar
+    jekyll-default-layout
+    jekyll-feed
+    jekyll-gist
+    jekyll-github-metadata
+    jekyll-mentions
+    jekyll-readme-index
+    jekyll-relative-links
+    jekyll-remote-theme
+    jekyll-seo-tag
+    jekyll-sitemap
+    jekyll-spaceship
+    kramdown-parser-gfm
+    netrc
+  ];
+}
