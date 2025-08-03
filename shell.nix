cb4c0f7b8ef93a51200712706b565361742fb3b4
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-08-03 23:08:46 +0800

    :art: Exclude some files

--- shell.nix
+++ shell.nix
@@ -1,22 +1,23 @@
 { pkgs ? import <nixpkgs> { } }:
 with pkgs;
-with rubyPackages;
 mkShell {
-  packages = [
-    jekyll-archives
-    jekyll-avatar
-    jekyll-default-layout
-    jekyll-feed
-    jekyll-gist
-    jekyll-github-metadata
-    jekyll-mentions
-    jekyll-readme-index
-    jekyll-relative-links
-    jekyll-remote-theme
-    jekyll-seo-tag
-    jekyll-sitemap
-    jekyll-spaceship
-    kramdown-parser-gfm
-    netrc
-  ];
+  packages =
+    with rubyPackages;
+    [
+      jekyll-archives
+      jekyll-avatar
+      jekyll-default-layout
+      jekyll-feed
+      jekyll-gist
+      jekyll-github-metadata
+      jekyll-mentions
+      jekyll-readme-index
+      jekyll-relative-links
+      jekyll-remote-theme
+      jekyll-seo-tag
+      jekyll-sitemap
+      jekyll-spaceship
+      kramdown-parser-gfm
+      netrc
+    ];
 }
